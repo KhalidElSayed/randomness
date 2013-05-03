af55afc768c348e03a79f0a85a0bf8b3ff768c71
Mohammadreza <live@taesiri.com>
2013-05-03 14:03:58 +0430

    [JavaScript]-{Linear Congruential Generator}

---
+++ JavaScript/Linear Congruential/LinearCongruential.js
@@ -0,0 +1,23 @@
+<script language="javascript" type="text/javascript">
+    <!-- Author: Mohammadreza Taesiri -->
+    Array.prototype.contains = function ( needle ) {
+        for (i in this) {
+            if (this[i] == needle) return true;
+        }
+        return false;
+    }
+
+    function lcg(m,seed,a,c){
+        var data = Array();
+        var _number = ((a * seed) + c )% m;
+        while (!data.contains(_number)) {
+            data.push(_number);
+            _number = (a * _number + c )% m;
+        }
+        var distribution = [];
+        for (var i = 0; i < data.length; i++) {
+            distribution.push(data[i]/m);
+        }
+        return distribution;
+    }
+</script>
